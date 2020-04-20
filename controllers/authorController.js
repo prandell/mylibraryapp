c3ad3bab694746df97c25a63f6bf4be4ebfe24a9
Patrick Randell <prandell@student.unimelb.edu.au>
2020-04-20 09:38:26 +0800

    Following Workshop

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,54 +1,37 @@
-// import the author model
-// i.e. provide the controller a link to the author model
-var authors = require("../models/author");
+const mongoose = require("mongoose");
 
+// import author model
+const Author = mongoose.model("author");
+
+    
 // function to handle a request to get all authors
-const getAllAuthors = (req, res) => {
-  res.send(authors); // return the list of authors
-};
-
-// function to handle a request to a particular author
-const getAuthorByID = (req, res) => {
-  // search for author in the database via ID
-  const author = authors.find(author => author.id === req.params.id);
-
-  if (author) {
-    // send back the author details
-    res.send(author);
-  } else {
-    // you can decide what to return if author is not found
-    // currently, an empty list will be returned
-    res.send([]);
+const getAllAuthors = async (req, res) => {
+    
+  try {
+    const all_authors = await Author.find();
+    return res.send(all_authors);
+  } catch (err) {
+    res.status(400);
+    return res.send("Database query failed");
   }
 };
-
-// function to handle request to add author
-const addAuthor = (req, res) => {
-  // extract info. from body
-  const author = req.body;
-
-  // add author to array
-  authors.push(author);
-  res.send(authors);
-};
+    
+  
+  
 
 // function to modify author by ID
-const updateAuthor = (req, res) => {
-  const new_author = req.body;
+const updateAuthor = async (req, res) => {
+  res.send("Working on this feature");
+};
 
-  // search for author in the database via ID
-  const author = authors.find(author => author.id === req.params.id);
-  if (!author) {
-	  // cannot be found
-	  return res.send([]);
-  }
+// function to add author
+const addAuthor = async (req, res) => {
+ res.send("Working on this feature");
+};
 
-  // now merge new_author into the original author object
-  // it is assumed that user input is well-formed (a dangerous assumption)
-  Object.assign(author, new_author);
-
-  // return updated author
-  res.send(author);
+// function to get author by id
+const getAuthorByID = (req, res) => {
+  res.send("Working on this feature");
 };
 
 // remember to export the functions
